b828fb42575fc02b0ea25ce4c4f9417030051dd0
OOOIOOOIO <polite159@gmail.com>
2025-01-06 21:51:34 +0900

    replyController swagger 작성

--- src/main/java/com/sh/trippy/domain/reply/api/ReplyController.java
+++ src/main/java/com/sh/trippy/domain/reply/api/ReplyController.java
@@ -1,11 +1,15 @@
 package com.sh.trippy.domain.reply.api;
 
 
+import com.sh.trippy.global.log.LogTrace;
+import com.sh.trippy.global.resolver.token.userinfo.UserInfoFromHeader;
+import com.sh.trippy.global.resolver.token.userinfo.UserInfoFromHeaderDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Trip Reply Controller", description = "Trip Reply API")
 @Slf4j
@@ -13,4 +17,73 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/trip")
 public class ReplyController {
+
+
+    @Operation(
+            summary = "여행에 대한 댓글 조회 API",
+            description = "여행에 대한 댓글 리스트로 조회"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "여행에 대해 댓글 조회 성공 후, 댓글 정보 리스트로 리턴"
+    )
+    @LogTrace
+    @GetMapping("/{tripId}/reply")
+    public String getTripReply(@UserInfoFromHeader UserInfoFromHeaderDto userInfoFromHeaderDto){
+
+
+        return "success";
+    }
+
+
+    @Operation(
+            summary = "여행에 대한 댓글 생성 API",
+            description = "여행에 대한 댓글 생성"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "여행에 대해 댓글 생성 성공 후, success(String) 리턴"
+    )
+    @LogTrace
+    @PostMapping("/{tripId}/reply")
+    public String createTripReply(@UserInfoFromHeader UserInfoFromHeaderDto userInfoFromHeaderDto){
+
+
+        return "success";
+    }
+
+    @Operation(
+            summary = "여행에 대한 댓글 수정 API",
+            description = "여행에 대한 수정 생성"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "여행에 대해 댓글 수정 성공 후, success(String) 리턴"
+    )
+    @LogTrace
+    @PutMapping("/{tripId}/reply/{replyId}")
+    public String updateTripReply(@UserInfoFromHeader UserInfoFromHeaderDto userInfoFromHeaderDto){
+
+
+        return "success";
+    }
+
+    @Operation(
+            summary = "여행에 대한 댓글 삭제 API",
+            description = "여행에 대한 삭제 생성"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "여행에 대해 댓글 삭제 성공 후, success(String) 리턴"
+    )
+    @LogTrace
+    @DeleteMapping("/{tripId}/reply/{replyId}")
+    public String deleteTripReply(@UserInfoFromHeader UserInfoFromHeaderDto userInfoFromHeaderDto){
+
+
+        return "success";
+    }
+
+
+
 }
